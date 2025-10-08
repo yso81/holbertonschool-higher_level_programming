8467077245efe27a6d8bf2e0fc819f32f1f9bc91
Yannick <yannicksookree@gmail.com>
2025-10-08 11:45:43 +0400

    matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print(element, end=' ')
+        print()
+
+
+if __name__ == "__main__":
+
+    matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
