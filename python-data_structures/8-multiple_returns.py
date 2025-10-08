bdc4ccaa42250592f1bea9ecd6bf4222997c1a7a
Yannick <yannicksookree@gmail.com>
2025-10-08 17:57:20 +0400

    upd

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+    sentence = ""
+    result = [length, first]
+
+    return result
+
+
+if __name__ == "__main__":
+
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
