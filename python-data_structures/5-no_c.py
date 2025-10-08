44d7008c06997d1203c3692b8dda3d8e5dac8861
Yannick <yannicksookree@gmail.com>
2025-10-08 11:13:14 +0400

    no c

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+    return new_string
+
+
+if __name__ == "__main__":
+
+    my_string = "hello cunts C you !"
+
+    print(no_c(my_string))
