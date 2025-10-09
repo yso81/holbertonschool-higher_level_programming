07a2f6fa59e01b1e249551d32cbcc96e7f70f270
Yannick <yannicksookree@gmail.com>
2025-10-09 14:43:03 +0400

    upd

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,14 +3,15 @@ def delete_at(my_list=[], idx=0):
     while idx < 0 and idx >= len(my_list):
         return my_list
 
-    del my_list[idx]
+        del my_list[idx]
+    
     return my_list
 
 
 if __name__ == "__main__":
 
     my_list = [1, 2, 3, 4, 5]
-    idx = 3
+    idx = 6
     new_list = delete_at(my_list, idx)
     print(new_list)
     print(my_list)
