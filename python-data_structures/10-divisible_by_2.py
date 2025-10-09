d4b2e50ed37be26c9f9a4b63bf61a684b22acc7d
Yannick <yannicksookree@gmail.com>
2025-10-09 11:28:46 +0400

    divisible by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    list_result = []
+    for number in my_list:
+        if (number % 2) == 0:
+            list_result.append(True)
+        else:
+            list_result.append(False)
+    return list_result
+
+
+if __name__ == "__main__":
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_result = divisible_by_2(my_list)
+
+    i = 0
+    while i < len(list_result):
+        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+        i += 1
