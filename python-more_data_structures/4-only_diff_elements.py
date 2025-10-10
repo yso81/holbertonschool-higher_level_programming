4ddd7365888add18f283fdf770094ded3e8a4147
Yannick <yannicksookree@gmail.com>
2025-10-10 11:30:56 +0400

    symmetric difference

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    uniq_elements = set_1 | set_2
+    uniq_elements = (set_1.symmetric_difference(set_2))
+
     return uniq_elements
+
 
 if __name__ == "__main__":
 
