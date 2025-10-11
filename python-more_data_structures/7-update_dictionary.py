920c7d94afb9b92beaf702b165955c3ecaf759a2
Yannick <yannicksookree@gmail.com>
2025-10-11 10:12:26 +0400

    upd

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,7 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
 
+
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
