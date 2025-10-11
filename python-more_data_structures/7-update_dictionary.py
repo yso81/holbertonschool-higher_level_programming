8266d8d986fa545efbcfeaf46a9793b8aaecc1f7
Yannick <yannicksookree@gmail.com>
2025-10-11 10:13:24 +0400

    upd

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -12,7 +12,7 @@ def print_sorted_dictionary(a_dictionary):
 
 if __name__ == "__main__":
 
-    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
     print("--")
