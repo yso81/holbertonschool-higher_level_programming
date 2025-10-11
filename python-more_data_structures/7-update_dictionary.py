96a10476551672514ed12abb7198f98ef16da943
Yannick <yannicksookree@gmail.com>
2025-10-11 10:08:35 +0400

    upd dict

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
+
+
+if __name__ == "__main__":
+
+    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+
+    print("--")
+    print("--")
+
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
