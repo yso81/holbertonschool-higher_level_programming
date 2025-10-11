449ecdd07536c4307d3e9bf88aecbe662963454a
Yannick <yannicksookree@gmail.com>
2025-10-11 14:01:56 +0400

    upd

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    sum = 0
+    for i in range(len(roman_string)):
+        current_value = roman_map[roman_string[i]]
+        if i + 1 < len(roman_string) and current_value < roman_map[roman_string[i+1]]:
+            sum -= current_value
+        else:
+            sum += current_value
+    return sum
+
+
+if __name__ == "__main__":
+
+    roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
