35c3faf54d197ab54becb7b7c3687d0afe661812
Yannick <yannicksookree@gmail.com>
2025-10-11 10:42:59 +0400

    mul by 2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    a_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
+    return a_dictionary
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
