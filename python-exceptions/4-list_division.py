b3e3e68690a7ba67c832893495d9590e4268fc32
Yannick <yannicksookree@gmail.com>
2025-10-12 17:30:47 +0400

    upd

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+
     new_list = []
+
     for i in range(list_length):
+
         result = 0
         try:
             value_1 = my_list_1[i]
@@ -12,25 +15,14 @@ def list_division(my_list_1, my_list_2, list_length):
             else:
                 try:
                     result = value_1 / value_2
+                    
                 except ZeroDivisionError:
                     print("Division by 0")
+
         except IndexError:
             print("Out of range")
+
         finally:
             new_list.append(result)
+
     return new_list
-
-
-if __name__ == "__main__":
-
-    my_l_1 = [10, 8, 4]
-    my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    print("--")
-
-    my_l_1 = [10, 8, 4, 4]
-    my_l_2 = [2, 0, "H", 2, 7]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
