39cbded69d6f48ae9d41c731f7492de872d6e42c
Yannick <yannicksookree@gmail.com>
2025-10-12 13:41:17 +0400

    upd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+
     count = 0
     try:
         for i in range(x):
@@ -12,15 +13,3 @@ def safe_print_list(my_list=[], x=0):
     else:
         print ("nil")
     return count
-
-
-if __name__ == "__main__":
-
-    my_list = [1, 2, 3, 4, 5]
-    
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
