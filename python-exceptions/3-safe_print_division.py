e7b4c29edacaf16d64993f638ea3c69e57ff4eab
Yannick <yannicksookree@gmail.com>
2025-10-12 16:51:11 +0400

    upd

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
-        print("{}".format(result))
+        
     except ZeroDivisionError:
-            pass
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+    return result
 
 
 if __name__ == "__main__":
