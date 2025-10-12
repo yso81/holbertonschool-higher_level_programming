bbfc7d0b2eea5d0a0a07ca66ebf8fe748c93464d
Yannick <yannicksookree@gmail.com>
2025-10-12 17:46:49 +0400

    upd

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
+            if type(my_list[i]) is int:
                 print("{}".format(my_list[i]), end="")
                 count += 1
     except TypeError:
