ad73fd2670caed09aa0c225b55ae933c31b1298b
Yannick <yannicksookree@gmail.com>
2025-10-14 15:42:47 +0400

    upd

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module creates a Square class.
+"""
+
+class Square:
+    """
+    Defines a square class with Private instance attribute: Size
+    """
+    
+    def __init__(self, size):
+        """
+        Initialises a New square instance.
+
+        Args: Size
+        """
+        self.__size = size
