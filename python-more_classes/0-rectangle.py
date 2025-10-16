3006bbe206f8cfad488df53678c55b4204a0ba11
Yannick <yannicksookree@gmail.com>
2025-10-16 14:14:04 +0400

    upd

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module creates an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    an empty class that defines a rectangle.
+    """
+
+
+if __name__ == "__main__":
+
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
