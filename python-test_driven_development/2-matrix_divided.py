9222d5a56ef73efbc9f7737ed38eea3c80cb8260
Yannick <yannicksookree@gmail.com>
2025-10-18 19:42:50 +0400

    upd

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,7 @@
     function that divides all elements of a matrix
 """
 
+
 def matrix_divided(matrix, div):
     """
     All elements of the matrix should be divided by div
@@ -24,27 +25,30 @@ def matrix_divided(matrix, div):
     if not isinstance(matrix, list):
         if not (isinstance(row, list) for row in matrix):
             raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-    
+                "matrix must be a matrix (list of lists) of integers/floats")
+
     if not matrix:
         return []
-    
+
     row_length = len(matrix[0])
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) "
+                    "of integers/floats")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = []
     for row in matrix:
         new_row = []
@@ -57,6 +61,4 @@ def matrix_divided(matrix, div):
 if __name__ == "__main__":
 
     import doctest
-    doctest.testfile("tests/2-matrix_divided.txt", verbose = True)
-
-
+    doctest.testfile("tests/2-matrix_divided.txt", verbose=True)
