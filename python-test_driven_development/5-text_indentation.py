6f18f5b6588c785061abde5e9870467bca40c3f6
Yannick <yannicksookree@gmail.com>
2025-10-20 10:34:37 +0400

    upd

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    i = 0
+    while i < len(text):
+        result += text[i]
+        if text[i] in ['.', '?', ':']:
+            result += "\n\n"
+
+            while i + 1 < len(text) and text[i + 1] == ' ':
+                i += 1
+         i += 1
+
+    for line in result.split('\n'):
+        print(line.strip())
+
+
+if __name__ == "__main__":
+
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt", verbose=True)
