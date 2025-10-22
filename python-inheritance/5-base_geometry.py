fd352d43696a9f6634041970ee8dec016db14e9f
Yannick <yannicksookree@gmail.com>
2025-10-22 13:51:55 +0400

    upd

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+An empty class named BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    An empty base class for geometry-related operations.
+    """
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A class named BaseGeometry with an unimplemented area method
+"""
+
+class BaseGeometry:
+    """
+    Includes an area method that raises an Exception if not implemented by a subclass
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
