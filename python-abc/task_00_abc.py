59da51d9ff34abafaa06a5785c3778b7b2627e03
Yannick <yannicksookree@gmail.com>
2025-10-24 12:50:14 +0400

    upd

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import abc
+
+class Animal(abc.ABC):
+    @abc.abstractmethod
+    def sound(self):
+        """
+        Abstract that defines the sound made by the animal
+        """
+        pass
+
+class Dog(Animal):
+    """
+    Implementing the dog subclass with sound method
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    Implementing the cat subclass with sound method
+    """
+    def sound(self):
+        return "Meow"
+
+
+if __name__ == "__main__":
+    print("--- Testing the Animal ABC and its subclasses ---")
+
+    try:
+        """
+        Instantiating abstract class Animal will raise a TypeError
+        """
+        animal = Animal()
+    except TypeError as e:
+        print(f"ZError: {e}")
+
+
+    """
+    Instantiating subclasses Dog and Cat
+    """
+    dog = Dog()
+    cat = Cat()
+
+
+    """
+    Printing the sound method on the instances
+    """
+    print(f"Dog makes: {dog.sound()}")
+    print(f"Cat makes: {cat.sound()}")
+
+
+    """
+    Polymorphism
+    """
+    animals = [Dog(), Cat()]
+    for animal in animals:
+        print(f"Animal makes : {animal.sound()}")
