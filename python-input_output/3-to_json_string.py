fda6502097aed5a381f2f0e44a1ea584bdf33935
Yannick <yannicksookree@gmail.com>
2025-10-28 20:05:58 +0400

    upd

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Returns the JSON representation of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be serialized to a JSON string.
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
