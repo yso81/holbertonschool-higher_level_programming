e7e55365590bad2181461659fa331e5312f98b29
Yannick <yannicksookree@gmail.com>
2025-10-28 19:57:34 +0400

    upd

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file (UTF8) and returns the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters
+
+    Args:
+        filename (str): The name of the file to write to
+        text (str): The string to write to the file
+
+    Returns:
+        int: The number of characters written to the file
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
