b39a7470aa1dd605cfdd04110a56c1f7715478df
Yannick <yannicksookree@gmail.com>
2025-10-29 13:11:08 +0400

    upd

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -9,16 +9,16 @@
 def class_to_json(obj):
     """
     Args:
-        obj: An instance of a Class. All attributes of the obj Class are serializable:
-             list, dictionary, string, integer and boolean
+        obj: An instance of a Class. All attributes of the obj Class are
+        serializable: list, dictionary, string, integer and boolean
 
     Returns:
         A dictionary representation of the object's serializable attributes
     """
     json_dict = {}
-    for attr_name in dir(obj):
-        if not attr_name.startswith('__') and not callable(getattr(obj, attr_name)):
-            attr_value = getattr(obj, attr_name)
+    for attr in dir(obj):
+        if not attr.startswith('__') and not callable(getattr(obj, attr)):
+            attr_value = getattr(obj, attr)
             if isinstance(attr_value, (list, dict, str, int, bool)):
-                json_dict[attr_name] = attr_value
+                json_dict[attr] = attr_value
     return json_dict
