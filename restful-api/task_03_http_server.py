8a434611ca7d82579a774a6c4b5ca821e1cf28ac
Yannick <yannicksookree@gmail.com>
2025-11-06 11:14:56 +0400

    upd

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -11,7 +11,13 @@ PORT = 8000
 
 class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/data":
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+
+        elif self.path == "/data":
 
             data = {"name": "John", "age": 30, "city": "New York"}
             json_data = json.dumps(data)
@@ -21,7 +27,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(json_data, "utf-8"))
 
-        elif self.path == "/info": # New condition for /info endpoint
+        elif self.path == "/info":
             info_data = {"version": "1.0", "description": "A simple API built with http.server"}
             json_info = json.dumps(info_data)
 
@@ -38,8 +44,9 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port", PORT)
-    print(f"- Visit http://localhost:{PORT} for the plain text API.")
+    print(f"- Visit http://localhost:{PORT}/ for the plain text API.")
     print(f"- Visit http://localhost:{PORT}/data for the JSON data.")
     print(f"- Visit http://localhost:{PORT}/info for the API info JSON data.")
+    print(f"- Visit http://localhost:{PORT}/undefined for a 404 Not Found response.")
 
     httpd.serve_forever()
