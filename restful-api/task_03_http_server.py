76bf7304c40440baaf57ee547b82d913e8862f5f
Yannick <yannicksookree@gmail.com>
2025-11-06 11:29:42 +0400

    upd

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -35,13 +35,6 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(bytes(json_info, "utf-8"))
-    
-        elif self.path == "/status":
-
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(bytes(json_info, "utf-8"))
 
         else:
             self.send_response(404)
