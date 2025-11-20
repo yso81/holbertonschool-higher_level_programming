e400a6a969602f5bf80f2a042a4e90b6f94d7fab
Yannick <yannicksookree@gmail.com>
2025-11-20 17:23:24 +0400

    upd

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- script to insert the new row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- script to display the number of records with id = 89 in first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
