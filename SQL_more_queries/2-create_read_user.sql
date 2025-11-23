a87e4966d36fdf8389e80013f488393a0c20d8e6
Yannick <yannicksookree@gmail.com>
2025-11-23 12:02:23 +0400

    upd

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,5 @@
 only SELECT privilege granted to user_0d_2 in the database hbtn_0d_2
 user_0d_2 password should be set to user_0d_2_pwd */
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2; CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'; GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
