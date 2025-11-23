833fc22e1c90092f4516b3b8f1cad93fe2defe83
Yannick <yannicksookree@gmail.com>
2025-11-23 12:04:28 +0400

    upd

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,3 @@
-/* script that creates the database hbtn_0d_2 and the user user_0d_2,
-only SELECT privilege granted to user_0d_2 in the database hbtn_0d_2
-user_0d_2 password should be set to user_0d_2_pwd */
+-- script that creates the database hbtn_0d_2 and the user user_0d_2, only SELECT privilege granted to user_0d_2 in the database hbtn_0d_2 user_0d_2 password should be set to user_0d_2_pwd
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2; CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'; GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'; FLUSH PRIVILEGES;
