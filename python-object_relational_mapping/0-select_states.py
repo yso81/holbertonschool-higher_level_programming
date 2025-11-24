da1603275c5aff1732d68822172414b166b3b79e
Yannick <yannicksookree@gmail.com>
2025-11-24 13:45:25 +0400

    upd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,7 @@ Lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+
 mysql_username = sys.argv[1]
 mysql_password = sys.argv[2]
 database_name = sys.argv[3]
@@ -25,7 +26,7 @@ states_id = c.fetchall()
 
 for row in states_id:
     print(row)
-    
+
 
 c.close()
 db.close()
