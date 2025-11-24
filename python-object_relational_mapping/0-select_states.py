d37a2d6fabd17226baa6921d289b976187b9d4de
Yannick <yannicksookree@gmail.com>
2025-11-24 14:30:17 +0400

    upd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,13 +14,8 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    # Connect to MySQL server on localhost port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
 
     c = db.cursor()
 
