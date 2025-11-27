dc1cfbf871d249e6e84852d102b10c11733be529
Yannick <yannicksookree@gmail.com>
2025-11-27 14:53:03 +0400

    upd

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Get arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    # Using 'mysql+mysqldb' driver, localhost, and default port 3306
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        mysql_username, mysql_password, database_name), pool_pre_ping=True)
+
+    # Create a configured Session class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the first State object ordered by id
+    # The method .first() retrieves only the first result or None
+    state = session.query(State).order_by(State.id).first()
+
+    # Print results
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
