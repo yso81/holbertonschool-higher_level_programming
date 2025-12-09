4a97d8ac34193cfa16ae45245154acb4a30da6e6
Yannick <yannicksookree@gmail.com>
2025-12-09 13:47:07 +0400

    upd

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,8 @@
+// Select the header element using document.querySelector
+const headerElement = document.querySelector('header');
+
+// Check if the element was found before trying to change its style
+if (headerElement) {
+    // Update the color style to red (#FF0000)
+    headerElement.style.color = '#FF0000';
+}
